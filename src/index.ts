350fdcecf3406d5fc0e8ef32b416b41a8aaa5665
carloza19 <cloza458@gmail.com>
2024-04-10 17:33:26 -0300

    Add db connection

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 import "dotenv/config"
 import express from 'express'
 import cors from 'cors'
-import {productsRouter} from './routes'
+import db from "./config/mongo"
+import { productsRouter } from './routes'
 
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
+db().then(() => console.log('db connection ready'));
 
-app.use('/v1', productsRouter)
 
-app.listen(PORT, ()=> {
+app.use('/v1', productsRouter);
+
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
